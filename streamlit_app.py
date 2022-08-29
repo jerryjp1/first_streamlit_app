67ddefd5d4f3c947cf7d537174412bac5d4ae514
jerryjp1 <112389652+jerryjp1@users.noreply.github.com>
2022-08-29 15:59:46 +0530

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Lunch - Menu')
+streamlit.text('1. Steamed Jeera Rice')
+streamlit.text('2. Dal tadka')
+streamlit.text('3. Chicken Khurma')
