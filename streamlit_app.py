e9ff2ce8ad4111bb17331c94baf2e9cf3d0e77ed
jerryjp1 <112389652+jerryjp1@users.noreply.github.com>
2022-09-02 23:34:58 +0530

    fruit vice api response check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruity_vice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
@@ -17,3 +19,4 @@ streamlit.text('🥣 Dal tadka')
 streamlit.text('🐔 Chicken Tandoor')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(fruits_to_show)
+streamlit.text(fruityvice_response)
