6983acf26ccb2b3c3a795c118df275c36d243f6e
jerryjp1 <112389652+jerryjp1@users.noreply.github.com>
2022-09-02 23:42:24 +0530

    panda used to beautify json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit
 import pandas
-import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
@@ -21,5 +19,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(fruits_to_show)
 
 # Display fruity vice api data
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+# flattens the json using pandas 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the json in tabular
+streamlit.dataframe(fruityvice_normalized)
