2c5ff08c1404c9408354d466a884abbaa47c3806
jerryjp1 <112389652+jerryjp1@users.noreply.github.com>
2022-09-03 15:28:34 +0530

    fruit load list data display

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,9 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+# my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
+streamlit.text("List of fruits :")
+# streamlit.text(my_data_row) displays -> {a,b,c}
+streamlit.dataframe(my_data_rows)
