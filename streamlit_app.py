e3a3d7ae79ed7cbdd816273a6227a2c1d50cbe97
jerryjp1 <112389652+jerryjp1@users.noreply.github.com>
2022-09-05 12:56:50 +0530

    try except added

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,20 @@ streamlit.dataframe(fruits_to_show)
 import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get info.!')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-# streamlit.text(fruityvice_response.json())
-# flattens the json using pandas 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the json in tabular
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 # snowflake connection added
